f5daf5bd3d7815c3d2a53bf5e5317055a1b5fa13
Hetchell <TBNRdavrojeffrey112@gmail.com>
2023-03-03 17:27:00 +0800

    Add check method and translate some instructions.

--- GameMain/Program.cs
+++ GameMain/Program.cs
@@ -1,16 +1,31 @@
 ﻿using Game;
 using Survivalcraft.Game.ModificationHolder;
+using System.IO;
 
 namespace GameMain
 {
     class Program
     {   
+        private static bool state = false;
         [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Test");
-            ModificationsHolder.UpdateCommand();
-            ProgramGame.MainMethod();
+            if(state){
+                Console.WriteLine("Hello, World! Test");
+                ModificationsHolder.UpdateCommand();
+                ProgramGame.MainMethod();
+            } else {
+                CheckMethod();
+            }
+        }
+
+        static void CheckMethod(){
+            string txtfile = "GameMain\\Notes\\InstructionListFormat.txt";
+            string currDir = Directory.GetCurrentDirectory();
+            string path = string.Concat(currDir, "\\", txtfile);
+            Console.WriteLine(string.Format("Your current file location is at {0}", path));
+            string[] contains = File.ReadAllLines(path);
+            Console.WriteLine(contains[98]);
         }
     }
 }
